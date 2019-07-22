ed7ae00b69c947eb5885ef8d2e84342f7f31b370
ConnorWolanski <ronnocool222@gmail.com>
2019-07-21 22:09:23 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TeamFightTacticsBot',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Connor',
+    author_email='',
+    description=''
+)
