545ad5715428ae90bbf31d8bfeebd42f64b0a3a9
Scoutzknifez <Cody@4alexanders.com>
2019-07-27 21:46:23 -0700

    Uploaded ItemEnhancements.

---
+++ TeamFightTacticsBot/Enumerators/ItemEnhancements.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class ItemEnhancements(Enum):
+    ATTACK_DAMAGE = 20
+    ATTACK_SPEED = .2
+    ABILITY_POWER = 20
+    STARTING_MANA = 20
+    ARMOR = 20
+    MAGIC_RESIST = 20
+    HEALTH = 200
+    SPATULA = 0
